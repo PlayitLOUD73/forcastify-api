873fd70503cf8927c324f1c78b0552ec3c49c80d
PlayitLOUD73 <github@lukewelborn.com>
2022-09-15 09:55:41 -0400

    Tried to combine into one file

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-import { getLocationFromSearch } from "../api";
+var PAT = process.env.LocationIQKey;
+
+function getLocationFromSearch(props) {
+  let url =
+    "https://us1.locationiq.com/v1/search?key=" +
+    PAT +
+    "&q=" +
+    props +
+    "&format=json";
+  let response = fetch(url);
+  if (response.ok) {
+    var json = response.json();
+    console.log(json[0].lat + "\n" + json[0].lon);
+  } else {
+    alert("HTTP-Error: " + response.status);
+  }
+
+  return { lat: json[0].lat, lon: json[0].lon };
+}
 
 var http = require("http");
 http
