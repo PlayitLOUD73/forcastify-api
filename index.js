718143d4a8bb64a550c6568dd2fbdc7b0e16b611
PlayitLOUD73 <github@lukewelborn.com>
2022-09-15 10:27:59 -0400

    Testing sending back response

--- index.js
+++ index.js
@@ -25,10 +25,9 @@ http
   .createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`);
     if (req.url === "/api/location-request") {
-      res.write("API");
-      getLocationFromSearch("206 Green Chase W");
+      var json = getLocationFromSearch("206 Green Chase W");
+      res.write(json);
     }
-    res.write("Success!");
     res.end();
   })
   .listen(process.env.PORT || 3000);
