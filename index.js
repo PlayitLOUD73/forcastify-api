934e53e206927c37607cf822256c8773c55ab6d7
PlayitLOUD73 <github@lukewelborn.com>
2022-09-15 09:37:08 -0400

    Testing link changes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Success!');
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`);
+    if (req.url === "/api") {
+      res.write("API");
+    }
+    res.write("Success!");
     res.end();
-}).listen(process.env.PORT || 3000);
+  })
+  .listen(process.env.PORT || 3000);
