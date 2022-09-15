e68ce09290257666fd61fdbc69ab25dce0f205a8
PlayitLOUD73 <github@lukewelborn.com>
2022-09-15 09:44:56 -0400

    Added an API call.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var http = require("http");
 http
   .createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`);
-    if (req.url === "/api") {
+    if (req.url === "/api/location-request") {
       res.write("API");
+      getLocationFromSearch("206 Green Chase W");
     }
     res.write("Success!");
     res.end();
